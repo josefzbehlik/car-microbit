a10a5272b36978caaf6c4909fc17bdc7669fdc2b
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-04-27 11:32:00 +0200

    už tě objede

--- main.ts
+++ main.ts
@@ -6,11 +6,13 @@ let rightldx = 0;
 const speed = [0, 50, 100]
 
 input.onButtonPressed(Button.A, function () {
-    PCAmotor.MotorRun(m1, -750)
+    PCAmotor.MotorRun(m1, -750)//speed od 0 do 255 (pod 100 to nejede)
+    PCAmotor.MotorRun(m4, 750)
 })
 
 input.onButtonPressed(Button.B, function () {
-    PCAmotor.MotorRun(m4, -750)
+    PCAmotor.MotorRun(m1, -300)
+    PCAmotor.MotorRun(m4, -100)
 })
 
 input.onButtonPressed(Button.AB, function () {
