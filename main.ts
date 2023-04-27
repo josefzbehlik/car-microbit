c691fdc7f2bff4e52819f190b6983229b5850cf3
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-04-27 11:14:52 +0200

    on sa točí

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
+let m1 = PCAmotor.Motors.M1
+let m4 = PCAmotor.Motors.M4
 
+let leftldx = 0;
+let rightldx = 0;
+const speed = [0, 50, 100]
+
+input.onButtonPressed(Button.A, function () {
+    PCAmotor.MotorRun(m1, -750)
+})
+
+input.onButtonPressed(Button.B, function () {
+    PCAmotor.MotorRun(m4, -750)
+})
+
+input.onButtonPressed(Button.AB, function () {
+    PCAmotor.MotorStopAll()
+    leftldx = rightldx = 0;
+})
