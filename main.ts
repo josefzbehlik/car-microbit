77cca1fad77c647d26288c1d17168aee457d4faa
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2023-05-03 13:35:21 +0200

    haes

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+/*
 let m1 = PCAmotor.Motors.M1
 let m4 = PCAmotor.Motors.M4
 
@@ -19,3 +20,56 @@ input.onButtonPressed(Button.AB, function () {
     PCAmotor.MotorStopAll()
     leftldx = rightldx = 0;
 })
+*/
+
+
+function pack (x:number, y:number, z:number): number 
+{
+    let xmod = x + 1024 //posund do kladného intervalu
+    xmod = Math.round(xmod / 8) //redukce intervalu
+
+//String.fromCharCode()
+//"@"\dfjhfg.charCodeAt(1)
+
+/* 
+function pack(cisla:[number]):cisla
+
+for (let i = 0; i < cisla.lenght; i++) {
+    console.log(cisla)
+}
+*/
+    return 0
+}
+
+input.onButtonPressed(Button.A, function(){
+
+    let x = 1022 // <-1024; 1023>
+    let y = -950
+    let z = 12
+
+
+
+
+    //radio.sendString("")
+    radio.sendNumber(pack(x, y, z))
+
+})
+
+radio.onReceivedNumber(function(receivedNumber: number){
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
