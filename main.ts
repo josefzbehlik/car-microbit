1bf5feedca3cfb883f6b24eac8266d6917b3fce5
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2023-05-12 13:54:49 +0200

    nice

--- main.ts
+++ main.ts
@@ -8,23 +8,27 @@ let rightldx = 0;
 const speed = [0, 50, 100]
 
 const serialnum = -1309627909
-    
-radio.onReceivedValue(function(name: string, value: number) {
+
+radio.onReceivedValue(function (name: string, value: number) {
     if (serialnum != radio.receivedPacket(RadioPacketProperty.SerialNumber)) return;
-  
-        if (name=="GO" && value==18) {
-            PCAmotor.MotorRun(m1, -215)
-            PCAmotor.MotorRun(m4, -255)
-        }    
 
-        if (name == "BACK" && value == 17) { 
-            PCAmotor.MotorRun(m1, 215)
-            PCAmotor.MotorRun(m4, 255)
-        }
-        
-        if (name=="STOP" && value==16) {
-            PCAmotor.MotorStopAll() 
-        }  
+    if (name == "GO" && value == 18) {
+        PCAmotor.MotorRun(m1, -215)
+        PCAmotor.MotorRun(m4, -255)
+    }
+
+    if (name == "BACK" && value == 17) {
+        PCAmotor.MotorRun(m1, 215)
+        PCAmotor.MotorRun(m4, 255)
+    }
+
+    if (name == "LEFT" && value == 16) {
+        PCAmotor.MotorRun(m1, -215)
+        PCAmotor.MotorRun(m4, 255)
+    }
+
+    if (name == "RIGHT" && value == 15) {
+        PCAmotor.MotorRun(m1, 215)
+        PCAmotor.MotorRun(m4, -255)
+    }
 })
-
-
