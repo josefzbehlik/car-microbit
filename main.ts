80d74291ec95bc167e3e45d3d2f1666102db0a95
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2023-05-14 17:00:06 +0200

    nice

--- main.ts
+++ main.ts
@@ -3,31 +3,27 @@ radio.setGroup(13)
 let m1 = PCAmotor.Motors.M1
 let m4 = PCAmotor.Motors.M4
 
-let leftldx = 0;
-let rightldx = 0;
-const speed = [0, 50, 100]
-
 const serialnum = -1309627909
 
 radio.onReceivedValue(function (name: string, value: number) {
     if (serialnum != radio.receivedPacket(RadioPacketProperty.SerialNumber)) return;
 
-    if (name == "STOP" && value == 15) {
-        PCAmotor.MotorStopAll
-    }
-    
-    if (name == "GO" && value == 18) {
+    if (name == "RIGHT" && value == 16) {
         PCAmotor.MotorRun(m1, -215)
+        PCAmotor.MotorRun(m4, 255)
+    }
+    if (name == "LEFT" && value == 15) {
+        PCAmotor.MotorRun(m1, 215)
         PCAmotor.MotorRun(m4, -255)
     }
-
-    if (name == "BACK" && value == 17) {
+    if (name == "BACK" && value == 14) {
+        basic.pause(500)
         PCAmotor.MotorRun(m1, 215)
         PCAmotor.MotorRun(m4, 255)
     }
-
-    if (name == "RIGHT" && value == 16) {
-        PCAmotor.MotorRun(m1, 215)
+    if (name == "GO" && value == 13) {
+        basic.pause(500)
+        PCAmotor.MotorRun(m1, -215)
         PCAmotor.MotorRun(m4, -255)
     }
 })
