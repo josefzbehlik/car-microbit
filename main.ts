b8b9301f74b8b96cd86770c8f80b73a391c77d6e
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-05-23 16:34:53 +0200

    lehčí

--- main.ts
+++ main.ts
@@ -3,27 +3,43 @@ radio.setGroup(13)
 let m1 = PCAmotor.Motors.M1
 let m4 = PCAmotor.Motors.M4
 
+const M1_MAXSPEED = 235
+const M4_MAXSPEED = 255
+
+const LOWER_DEADZONE = 300
+const UPPER_DEADZONE = 700
+
 const serialnum = -1309627909
 
-radio.onReceivedValue(function (name: string, value: number) {
+radio.onReceivedString(function (receivedString: string) {
     if (serialnum != radio.receivedPacket(RadioPacketProperty.SerialNumber)) return;
+    let data = receivedString.split(";")
+    let x = parseInt(data[0]) - 1024
+    let y = parseInt(data[1]) - 1024
 
-    if (name == "RIGHT" && value == 16) {
-        PCAmotor.MotorRun(m1, -215)
-        PCAmotor.MotorRun(m4, 255)
+    let x_scale = Math.constrain(Math.abs(x), LOWER_DEADZONE, UPPER_DEADZONE) / (UPPER_DEADZONE - LOWER_DEADZONE)
+    let y_scale = Math.constrain(Math.abs(y), LOWER_DEADZONE, UPPER_DEADZONE) / (UPPER_DEADZONE - LOWER_DEADZONE)
+
+    
+    let M1Speed = 0
+    let M4Speed = 0
+
+    if (x > LOWER_DEADZONE) {
+        M1Speed -= M1_MAXSPEED * x_scale
+        M4Speed += M4_MAXSPEED * x_scale
+    } else if (x < -LOWER_DEADZONE) {
+        M1Speed += M1_MAXSPEED * x_scale
+        M4Speed -= M4_MAXSPEED * x_scale
     }
-    if (name == "LEFT" && value == 15) {
-        PCAmotor.MotorRun(m1, 215)
-        PCAmotor.MotorRun(m4, -255)
+    if (y > LOWER_DEADZONE) {
+        M1Speed += M1_MAXSPEED * y_scale
+        M4Speed += M4_MAXSPEED * y_scale
+    } else if (y < -LOWER_DEADZONE) {
+        M1Speed -= M1_MAXSPEED * y_scale
+        M4Speed -= M4_MAXSPEED * y_scale
     }
-    if (name == "BACK" && value == 14) {
-        basic.pause(500)
-        PCAmotor.MotorRun(m1, 215)
-        PCAmotor.MotorRun(m4, 255)
-    }
-    if (name == "GO" && value == 13) {
-        basic.pause(500)
-        PCAmotor.MotorRun(m1, -215)
-        PCAmotor.MotorRun(m4, -255)
-    }
+
+    PCAmotor.MotorRun(m1, M1Speed / 2)
+    PCAmotor.MotorRun(m4, M4Speed / 2)
+
 })
