9778c76b6841df9d44e3290ee10633d8713d1ee0
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-05-30 10:20:12 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -11,35 +11,46 @@ const UPPER_DEADZONE = 700
 
 const serialnum = -1309627909
 
-radio.onReceivedString(function (receivedString: string) {
-    if (serialnum != radio.receivedPacket(RadioPacketProperty.SerialNumber)) return;
-    let data = receivedString.split(";")
-    let x = parseInt(data[0]) - 1024
-    let y = parseInt(data[1]) - 1024
+// radio.onReceivedString(function (receivedString: string) {
+//     if (serialnum != radio.receivedPacket(RadioPacketProperty.SerialNumber)) return;
+//     let data = receivedString.split(";")
+//     let x = parseInt(data[0]) - 1024
+//     let y = parseInt(data[1]) - 1024
 
-    let x_scale = Math.constrain(Math.abs(x), LOWER_DEADZONE, UPPER_DEADZONE) / (UPPER_DEADZONE - LOWER_DEADZONE)
-    let y_scale = Math.constrain(Math.abs(y), LOWER_DEADZONE, UPPER_DEADZONE) / (UPPER_DEADZONE - LOWER_DEADZONE)
+//     let x_scale = Math.constrain(Math.abs(x), LOWER_DEADZONE, UPPER_DEADZONE) / (UPPER_DEADZONE - LOWER_DEADZONE)
+//     let y_scale = Math.constrain(Math.abs(y), LOWER_DEADZONE, UPPER_DEADZONE) / (UPPER_DEADZONE - LOWER_DEADZONE)
 
     
-    let M1Speed = 0
-    let M4Speed = 0
+//     let M1Speed = 0
+//     let M4Speed = 0
 
-    if (x > LOWER_DEADZONE) {
-        M1Speed -= M1_MAXSPEED * x_scale
-        M4Speed += M4_MAXSPEED * x_scale
-    } else if (x < -LOWER_DEADZONE) {
-        M1Speed += M1_MAXSPEED * x_scale
-        M4Speed -= M4_MAXSPEED * x_scale
-    }
-    if (y > LOWER_DEADZONE) {
-        M1Speed += M1_MAXSPEED * y_scale
-        M4Speed += M4_MAXSPEED * y_scale
-    } else if (y < -LOWER_DEADZONE) {
-        M1Speed -= M1_MAXSPEED * y_scale
-        M4Speed -= M4_MAXSPEED * y_scale
-    }
+//     if (x > LOWER_DEADZONE) {
+//         M1Speed -= M1_MAXSPEED * x_scale
+//         M4Speed += M4_MAXSPEED * x_scale
+//     } else if (x < -LOWER_DEADZONE) {
+//         M1Speed += M1_MAXSPEED * x_scale
+//         M4Speed -= M4_MAXSPEED * x_scale
+//     }
+//     if (y > LOWER_DEADZONE) {
+//         M1Speed += M1_MAXSPEED * y_scale
+//         M4Speed += M4_MAXSPEED * y_scale
+//     } else if (y < -LOWER_DEADZONE) {
+//         M1Speed -= M1_MAXSPEED * y_scale
+//         M4Speed -= M4_MAXSPEED * y_scale
+//     }
 
-    PCAmotor.MotorRun(m1, M1Speed / 2)
-    PCAmotor.MotorRun(m4, M4Speed / 2)
+//     PCAmotor.MotorRun(m1, M1Speed / 2)
+//     PCAmotor.MotorRun(m4, M4Speed / 2)
 
-})
+// })
+
+
+
+pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+
+
+
+
+
