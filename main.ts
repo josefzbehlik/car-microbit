f1fb54f14553c22d7e1b7309c9ab1befc98a7c31
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-06-12 11:23:15 +0200

    senzor stojí

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ let whiteLine = 0
 let pinC = DigitalPin.P15
 let pinL = DigitalPin.P14 // zkontrolovat piny
 let pinR = DigitalPin.P13
+let pinShoot = DigitalPin.P2
+let pinEcho = DigitalPin.P1
 
 pins.setPull(pinC, PinPullMode.PullNone)
 pins.setPull(pinL, PinPullMode.PullNone)
@@ -27,6 +29,7 @@ basic.forever(function () {
     let c = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? false : true
     let l = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
     let r = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
+    let obstacle = sonar.ping(pinShoot, pinEcho, PingUnit.Centimeters, 50)
 
     if (c) {
        
@@ -40,5 +43,14 @@ basic.forever(function () {
         PCAmotor.MotorRun(PCAmotor.Motors.M1, -125)
         PCAmotor.MotorRun(PCAmotor.Motors.M4, 105)
     }
-}
-)
+    if (obstacle < 15 && obstacle > 0) {
+        PCAmotor.MotorStopAll()
+        basic.pause(300)
+        music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, -125)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 105)
+        basic.pause(300)
+        PCAmotor.MotorStopAll()
+
+    }
+})
