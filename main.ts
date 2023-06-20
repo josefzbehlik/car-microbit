a780a1fd9a0451a7ab187ce5494fbe733b202e93
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-06-20 09:54:04 +0200

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,56 +1,44 @@
-let autoModeEnabled = true
-let whiteLine = 0
+radio.setGroup(13)
+let serialnum = 111111111111
+radio.onReceivedString(function (receivedString: string) {
+    if (serialnum != radio.receivedPacket(RadioPacketProperty.SerialNumber)) return;
+    let data = receivedString.split(";")
+
+    let x = parseInt(data[0]) - 1024
+    let y = parseInt(data[1]) - 1024
+
+    let M1_MAXSPEED = 250
+    let M4_MAXSPEED = 250
+
+    let LOWER_DEADZONE = 300
+    let UPPER_DEADZONE = 600
+
+    let m1 = PCAmotor.Motors.M1
+    let m4 = PCAmotor.Motors.M4
+
+    let x_scale = Math.constrain(Math.abs(x), LOWER_DEADZONE, UPPER_DEADZONE) / (UPPER_DEADZONE - LOWER_DEADZONE)
+    let y_scale = Math.constrain(Math.abs(y), LOWER_DEADZONE, UPPER_DEADZONE) / (UPPER_DEADZONE - LOWER_DEADZONE)
 
 
-let pinC = DigitalPin.P15
-let pinL = DigitalPin.P14 // zkontrolovat piny
-let pinR = DigitalPin.P13
-let pinShoot = DigitalPin.P2
-let pinEcho = DigitalPin.P1
+    let M1Speed = 0
+    let M4Speed = 0
 
-pins.setPull(pinC, PinPullMode.PullNone)
-pins.setPull(pinL, PinPullMode.PullNone)
-pins.setPull(pinR, PinPullMode.PullNone)
-
-
-let c = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? false : true
-let l = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
-let r = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
-
-
-
-pins.setPull(pinC, PinPullMode.PullNone)
-pins.setPull(pinL, PinPullMode.PullNone)
-pins.setPull(pinR, PinPullMode.PullNone)
-
-//funkční
-basic.forever(function () {
-
-    let c = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? false : true
-    let l = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
-    let r = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
-    let obstacle = sonar.ping(pinShoot, pinEcho, PingUnit.Centimeters, 50)
-
-    if (c) {
-       
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -105)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, -120)
-    } else if (l) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, 105)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, -130)
+    if (x > LOWER_DEADZONE) {
+        M1Speed -= M1_MAXSPEED * x_scale
+        M4Speed += M4_MAXSPEED * x_scale
+    } else if (x < -LOWER_DEADZONE) {
+        M1Speed += M1_MAXSPEED * x_scale
+        M4Speed -= M4_MAXSPEED * x_scale
     }
-    else if (r) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -125)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, 105)
+    if (y > LOWER_DEADZONE) {
+        M1Speed += M1_MAXSPEED * y_scale
+        M4Speed += M4_MAXSPEED * y_scale
+    } else if (y < -LOWER_DEADZONE) {
+        M1Speed -= M1_MAXSPEED * y_scale
+        M4Speed -= M4_MAXSPEED * y_scale
     }
-    if (obstacle < 15 && obstacle > 0) {
-        PCAmotor.MotorStopAll()
-        basic.pause(300)
-        music.playTone(Note.C, music.beat(BeatFraction.Whole))
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -125)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, 105)
-        basic.pause(300)
-        PCAmotor.MotorStopAll()
 
-    }
+    PCAmotor.MotorRun(m1, M1Speed / 2)
+    PCAmotor.MotorRun(m4, M4Speed / 2)
+
 })
